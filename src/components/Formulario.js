2effe397d56e8cfe21f8d0ae2b3ab18b9da4db77
Juan Carlos Valdés <idevkingos@gmail.com>
2021-09-20 13:10:30 -0300

    feat: custom hook

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import styled from '@emotion/styled'
+import React from "react";
+import styled from "@emotion/styled";
+import useMoneda from "../hooks/useMoneda";
 
 const Boton = styled.input`
-    margin-top: 20px;
-    font-weight: bold;
-    font-size: 20px;
-    padding: 10px;
-    background-color: #66a2fe;
-    border: none;
-    width: 100%;
-    border-radius: 10px;
-    color: #FFF;
-    transition: background-color .3s ease;
-    &:hover {
-        background-color: #326ac0;
-        cursor: pointer;
-    }
-`
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 20px;
+  padding: 10px;
+  background-color: #66a2fe;
+  border: none;
+  width: 100%;
+  border-radius: 10px;
+  color: #fff;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #326ac0;
+    cursor: pointer;
+  }
+`;
 const Formulario = () => {
-    return (
-        <form>
-            <Boton type="submit" value="Calcular" />
-        </form>
-    )
-}
+  const MONEDAS = [
+      { codigo: "USD", nombre: "Dolar de Estados Unidos" },
+      { codigo: "MXN", nombre: "Peso Mexicano" },
+      { codigo: "EUR", nombre: "Euro" },
+      { codigo: "GBP", nombre: "Libra Esterlina" },
+      { codigo: "ARS", nombre: "Peso Argentino" },
+  ]
+  // utilizar useMoneda
+  const [moneda, SelectMonedas, updateState] = useMoneda("Elige tu moneda", "", MONEDAS);
+  return (
+    <form>
+      <SelectMonedas />
+      <Boton type="submit" value="Calcular" />
+    </form>
+  );
+};
 
-export default Formulario
+export default Formulario;
---
+++ src/hooks/useMoneda.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const useMoneda = (label, stateInicial, opciones) => {
+  // State de nuestro custom hook
+  const [state, updateState] = useState(stateInicial);
+  const Seleccionar = () => (
+    <>
+      <label>Moneda</label>
+      <select>
+        <option value="">- Seleccione -</option>
+        {opciones.map((opcion) => (
+          <option value={opcion.codigo}>{opcion.nombre}</option>
+        ))}
+      </select>
+    </>
+  );
+  // retornar el state, updateState y el componente
+  return [state, Seleccionar, updateState];
+};
+
+export default useMoneda;
